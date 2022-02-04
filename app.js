8a5c07380ccd80740e78d0a69623875337739f12
Aodhán Keane <78481710+aodhanocathain@users.noreply.github.com>
2022-02-04 06:43:50 +0000

    Add bias to commit scoring

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const fs = require("fs");
 const port = 3000;
 const app = express();
@@ -17,28 +17,59 @@ app.get("/", (_req, res) => {
   });
 });
 
+function commitScore(commit) {
+  /*
+  let score = 0;
+  commit["files"].forEach(function (item, index, arr) {
+    score = score + item["patch"].length;
+  });
+  */
+  console.log(commit);
+  return 1;
+}
+
 app.get("/visualise", async (_req, res) => {
+  const segments = _req.url.split("%2F"); //taken from the user's pasted link
+  let url = `https://api.github.com/repos/${segments[3]}/${segments[4]}/stats/contributors`;
+  let response = await dataGet(url);
 
-  const segments = _req.url.split("%2F")  //taken from the user's pasted link
-  const url = `https://api.github.com/repos/${segments[3]}/${segments[4]}/stats/contributors`
-  const response = await dataGet(url)
+  const scores = {};
 
-  const commitCounts = {}
+  response.forEach(async function (contributor, index, contributors) {
+    const firstWeek = 0;  let lastWeek = 0;
+    let totalCommits = 0
+    contributor["weeks"].forEach(function (week, i, weeks) {
+      if (week["c"] > 0) {
+        totalCommits = totalCommits + week['c']
+        lastWeek = i;
+      } //active this week
+    });
+    const avgWeeklyCommits =  totalCommits/(1+(lastWeek-firstWeek))
 
-  response.forEach(function (item, index, arr) {
-    commitCounts[`'${item['author']['login']}'`] = item['total']
-  })
+    let score = 0
+    //for each week where the contributor had work left to do
+    for(let i=0; i<=lastWeek; i++)
+    {
+      //figure out how different this week's commit count is from the average
+      const week = contributor['weeks'][i]
+      const commits = week['c']
+      const factor = 1/(1+(Math.abs(commits-avgWeeklyCommits)/avgWeeklyCommits))
+      //use this to weight that week's additions and deletions
+      score += (week['a'] + week['d']) * factor
+    }
+    scores[`'${contributor['author']['login']}'`] = score
+  });
 
   //scuffed reconstruction of a new page
-  let htmlstring = "" + fs.readFileSync("./chartpt1.txt")
-  htmlstring = htmlstring + `const names = [${Object.keys(commitCounts)}];\n`
-  htmlstring = htmlstring + `const values = [${Object.values(commitCounts)}];\n`
-  htmlstring = htmlstring + fs.readFileSync("./chartpt2.txt")
+  let htmlstring = "" + fs.readFileSync("./chartpt1.txt");
+  htmlstring = htmlstring + `const names = [${Object.keys(scores)}];\n`;
+  htmlstring = htmlstring + `const values = [${Object.values(scores)}];\n`;
+  htmlstring = htmlstring + fs.readFileSync("./chartpt2.txt");
 
-  fs.writeFileSync("response.txt", htmlstring)  //debugging purposes
+  fs.writeFileSync("response.txt", htmlstring); //debugging purposes
 
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(htmlstring)
+  res.write(htmlstring);
   res.end();
 });
 
