a42f1b8bdbe9884238eba5e2ddf8ef2d1e8ceede
Aodhán Keane <78481710+aodhanocathain@users.noreply.github.com>
2022-02-04 06:38:07 +0000

    Add chart to "visualise" page with Chart.js data

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-const express = require("express")
+const express = require("express");
 const fs = require("fs");
 const port = 3000;
+const app = express();
 
-const app = express()
-
-app.get('/', (_req, res) => {
+app.get("/", (_req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   fs.readFile("./index.html", function (error, data) {
     if (error) {
       res.writeHead(404);
       res.write("not found");
     } else {
-        res.write(data)
+      res.write(data);
     }
-    res.end()
+    res.end();
   });
 });
 
-app.get('/visualise', (_req, res) => {
-  console.log(_req.url)
+const stars = []
+const names = []
+app.get("/visualise", (_req, res) => {
+
+  const str = '' + fs.readFileSync("defaultdata.json")
+  JSON.parse(str)["items"].forEach(function (item, index, arr) {
+    names.push(`'${item["name"]}'`);
+    stars.push(item["stargazers_count"]);
+  })
+
+  let htmlstring = "" + fs.readFileSync("./chartpt1.txt")
+  htmlstring = htmlstring + `let names = [${names}];\n`
+  htmlstring = htmlstring + `let values = [${stars}];\n`
+  htmlstring = htmlstring + fs.readFileSync("./chartpt2.txt")
+  fs.writeFileSync("response.txt", htmlstring)
+
   res.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile("./index.html", function (error, data) {
-    if (error) {
-      res.writeHead(404);
-      res.write("not found");
-    } else {
-        res.write(data)
-    }
-    res.end()
-  });
+  res.write(htmlstring)
+  res.end();
 });
 
 app.listen(port, (error) => {
